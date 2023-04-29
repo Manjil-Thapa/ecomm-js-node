d4449d7591c4f0ecaa3db0c3388fcb8f355fc91c
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-04-30 02:12:53 +1000

    fixed user repo methods

--- repositories/users.js
+++ repositories/users.js
@@ -36,13 +36,53 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString('hex');
   }
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+  async update(id, attributes) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+    Object.assign(record, attributes);
+    await this.writeAll(records);
+  }
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.create({ email: 'test@test.com', password: 'password' });
-  const users = await repo.getAll();
-  console.log(users);
+  //   await repo.create({ email: 'test@test.com', password: 'password' });
+  //   const users = await repo.getAll();
+  //   const user = await repo.getOne('878387ce');
+  const user = await repo.getOneBy({
+    email: 'test@test.com',
+    id: '1ccafb83',
+  });
+  console.log(user);
 };
 
 test();
