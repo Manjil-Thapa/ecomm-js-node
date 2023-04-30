d6af479760eb2aba87765b88a26a459eaf1ce522
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-05-01 01:50:12 +1000

    added exp validator and added for signup route

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -10,23 +11,46 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  //   console.log(req.body);
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email is already in use');
-  }
+router.post(
+  '/signup',
+  [
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Must be a valid email')
+      .custom(async email => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error('Email already in use');
+        }
+      }),
+    check('password').trim().isLength({ min: 4, max: 20 }),
+    check('passwordConfirmation')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .custom((passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error('Passwords must match');
+        }
+      }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    const { email, password, passwordConfirmation } = req.body;
+    //   console.log(req.body);
 
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
+    if (password !== passwordConfirmation) {
+      return res.send('Passwords must match');
+    }
+    //create a user in our user repo to represent this person
+    const user = await usersRepo.create({ email, password });
+    // store id of that user inside users cookie
+    req.session.userId = user.id; // added by cookie-session
+    res.send('Account created');
   }
-  //create a user in our user repo to represent this person
-  const user = await usersRepo.create({ email, password });
-  // store id of that user inside users cookie
-  req.session.userId = user.id; // added by cookie-session
-  res.send('Account created');
-});
+);
 
 router.get('/signout', (req, res) => {
   req.session = null;
--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,12 +1,15 @@
+const layout = require('../layout');
+
 module.exports = () => {
-  // we dont really need the req obj
-  return `
-    <div>
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <button>Sign in</button>
-        </form>
-    </div>
-      `;
+  return layout({
+    content: `
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <button>Sign in</button>
+            </form>
+        </div>
+    `,
+  });
 };
