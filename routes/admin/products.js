53bf6e636dc17845b0d902f751f580985ee12ea0
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-05-01 23:52:48 +1000

    products repo created

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,12 +1,16 @@
 // create router, export router and wire up to index.js
 const express = require('express');
+const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
 
 const router = express.Router();
 
 // route handlers
 router.get('/admin/produts', (req, res) => {});
 // show products
-router.get('/admin/products/new', (req, res) => {});
+router.get('/admin/products/new', (req, res) => {
+  res.send(productsNewTemplate({})); // empty obj for possible errors
+});
 // create new products
 
 // submit products
