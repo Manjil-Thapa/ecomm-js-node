54204c878d36b86849f8342a8c63fbb4487c331a
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-05-04 01:36:16 +1000

    added styling to cart and working math for the cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
+
 const router = express.Router();
 
 // receive a post req to add an item to a cart
@@ -32,7 +35,20 @@ router.post('/cart/products', async (req, res) => {
   res.send('Item added to cart');
 });
 // receive get req to show all items in cart
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    // if somehow user ends up on cart bfo getting cart id, redirect
+    return res.redirect('/');
+  }
+  const cart = await cartsRepo.getOne(req.session.cartId);
 
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 // receive post req to delete an item from cart
 
 module.exports = router;
